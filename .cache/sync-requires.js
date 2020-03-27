4a1a4f41920467c412f44ddd9276527ede41d573
Joanna Wytrzes <joannawytrzes@gmail.com>
2020-03-27 17:26:31 +0100

    Branch init

---
+++ .cache/sync-requires.js
@@ -0,0 +1,15 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\joann\\Desktop\\projekty\\solfran\\.cache\\dev-404-page.js"))),
+  "component---src-pages-blog-js": hot(preferDefault(require("C:\\Users\\joann\\Desktop\\projekty\\solfran\\src\\pages\\Blog.js"))),
+  "component---src-pages-home-js": hot(preferDefault(require("C:\\Users\\joann\\Desktop\\projekty\\solfran\\src\\pages\\Home.js"))),
+  "component---src-pages-post-js": hot(preferDefault(require("C:\\Users\\joann\\Desktop\\projekty\\solfran\\src\\pages\\Post.js"))),
+  "component---src-pages-root-js": hot(preferDefault(require("C:\\Users\\joann\\Desktop\\projekty\\solfran\\src\\pages\\Root.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\joann\\Desktop\\projekty\\solfran\\src\\pages\\index.js")))
+}
+
